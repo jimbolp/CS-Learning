6167fc7067d6fdf632f217fb563134458edd65cb
Yavor <jimbolp@gmail.com>
2016-10-27 14:02:05 +0300

    object and classes

--- Distance between Points/Rectangle Position/RectanglePosition.cs
+++ Distance between Points/Rectangle Position/RectanglePosition.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 class Rectangle
 {
@@ -60,7 +59,7 @@ class Rectangle
 
 
 }
-class Program
+class RectanglePosition
 {
     static void Main(string[] args)
     {        
