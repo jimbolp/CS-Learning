d18d253619c4987026aacdf7042f61ac08f0db96
Yavor <jimbolp@gmail.com>
2016-11-01 08:50:17 +0200

    new project

---
+++ C# Programming Fundamentals/Objects and Classes/Book Library/BookLibrary.cs
@@ -0,0 +1,70 @@
+﻿//using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+class BookLibrary
+{
+    class Library
+    {
+        private string name;
+        public string Name { get; set; }
+        public List<Books> books = new List<Books>();
+        public void combineAuthors()
+        {
+            List<string> combinedAuthors = new List<string>();
+            for(int i = 0; i < books.Count; ++i)
+            {
+                if (!combinedAuthors.Contains(books[i].Author))
+                {                   
+                    combinedAuthors.Add(books[i].Author);
+                }
+            }
+            for (int i = 0; i < combinedAuthors.Count; ++i)
+            {
+                double price = 0.0;
+                for (int j = 0; j < books.Count; j++)
+                {
+                    if(books[j].Author == combinedAuthors[i])
+                    {
+                        price += books[j].Price;
+                    }
+                }
+                Console.WriteLine($"{combinedAuthors[i]} -> {price:f2}");
+
+            }           
+
+        }
+    }
+    class Books
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int ISBN { get; set; }
+        public double Price { get; set; }
+
+    }
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        Library myLibrary = new Library();
+        for (int i = 0; i < n; i++)
+        {
+            List<string> input = Console.ReadLine().Split(' ').ToList();
+            Books temp = new Books();
+            temp.Title = input[0];
+            temp.Author = input[1];
+            temp.Publisher = input[2];
+            temp.ReleaseDate = DateTime.ParseExact(input[3], "dd.MM.yyyy", CultureInfo.InvariantCulture);
+            temp.ISBN = int.Parse(input[4]);
+            temp.Price = double.Parse(input[5]);
+            myLibrary.books.Add(temp);
+        }        
+        myLibrary.combineAuthors();
+        //Console.WriteLine(myLibrary.books[0].ReleaseDate.ToShortDateString() + "\n" + myLibrary.books[0].ISBN.ToString("D10"));
+    }    
+}
---
+++ C# Programming Fundamentals/Objects and Classes/Mentor group/MentorGroup.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+class MentorGroup
+{
+    static void Main(string[] args)
+    {
+        List<Mentor> mentors = new List<Mentor>();
+        TakeInput(mentors, "mentor", "end of dates");
+        TakeInput(mentors, "comment", "end of comments");
+        
+        mentors = mentors.OrderBy(x => x.MentorName).ToList();
+        //Console.WriteLine();
+        foreach (Mentor m in mentors)
+        {
+            m.OrderAttendingsByDate();
+        }
+       
+        PrintFormatedOutput(mentors);
+    }
+
+    public static void TakeInput(List<Mentor> mentors, string inputType, string endString )
+    {
+        string temp = Console.ReadLine();
+        if (inputType == "mentor")
+        {
+            while (temp != endString)
+            {
+                string[] input = temp.Split(new[] {' ', ','}, StringSplitOptions.RemoveEmptyEntries);
+                //if (input.Length < 2)
+                //{
+                    //temp = Console.ReadLine();
+                    //continue;
+                //}
+                //else
+                //{
+                    AddMentor(input, mentors);
+                    temp = Console.ReadLine();
+                //}
+            }
+        }
+        else if (inputType == "comment")
+        {
+            while (temp != endString)
+            {
+                string[] input = temp.Split( new [] {'-'}, StringSplitOptions.RemoveEmptyEntries);
+                if (input.Length < 2)
+                {
+                    temp = Console.ReadLine();
+                    continue;
+                }
+                else
+                    AddComment(input, mentors);
+                //if (!AddComment(input, mentors))
+                  //  Console.WriteLine($"{input[0]} is not in the list of attending mentors");
+                temp = Console.ReadLine();
+            }
+        }
+    }
+
+    public static void PrintFormatedOutput(List<Mentor> listToPrint)
+    {
+        foreach (Mentor m in listToPrint)
+        {
+            Console.WriteLine($"{m.MentorName}{Environment.NewLine}Comments:");
+            foreach (string comment in m.Comment)
+            {
+                Console.WriteLine($"- {comment}");
+            }
+            Console.WriteLine("Dates attended:");
+            foreach (DateTime date in m.AttendingDateTimes)
+            {
+                Console.WriteLine($"-- {date.ToString("dd/MM/yyyy",CultureInfo.InvariantCulture)}");
+            }
+        }
+    }
+    public static bool AddComment(string[] input, List<Mentor> mentors)
+    {
+        foreach (Mentor m in mentors)
+        {
+            if (m.MentorName == input[0])
+            {
+                m.Comment.Add(input[1]);
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void AddMentor(string[] input, List<Mentor> mentors )
+    {
+        if (input.Length == 1)
+            mentors.Add(new Mentor(input[0]));
+        else
+        {
+            for (int i = 1; i < input.Length; i++)
+            {
+                bool exist = false;
+                int mIndex = 0;
+                foreach (Mentor m in mentors)
+                {
+                    if (m.MentorName == input[0])
+                    {
+                        mIndex = mentors.IndexOf(m);
+                        exist = true;
+                    }
+                }
+                if (exist)
+                {
+                    mentors[mIndex].AttendingDateTimes.Add(DateTime.ParseExact(input[i], "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None));
+                }
+                else
+                    mentors.Add(new Mentor(input[0], DateTime.ParseExact(input[i], "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None)));
+            }
+        }
+        
+    }
+    public class Mentor
+    {
+        public string MentorName { get; set; }
+        public List<DateTime> AttendingDateTimes { get; set; } = new List<DateTime>();
+        public List<string> Comment { get; set; } = new List<string>();
+
+        public Mentor(string name)
+        {
+            MentorName = name;
+        }
+        public Mentor(string name, DateTime attendingDateTimes)
+        {
+            MentorName = name;
+            AttendingDateTimes.Add(attendingDateTimes);
+        }
+
+        public Mentor(string name, string comment)
+        {
+            MentorName = name;
+            Comment.Add(comment);
+        }
+
+        public void OrderAttendingsByDate()
+        {
+            AttendingDateTimes = AttendingDateTimes.OrderBy(x => x).ToList();
+        }
+
+    }
+}
