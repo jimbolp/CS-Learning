a228f993e822488bcdeb6722a596fda43805ea0d
finetii <fineti@gmail.com>
2017-04-07 16:27:06 +0300

    Fifi

---
+++ PrintersDataWebMVC/PrintersDataWebMVC/Models/PrinterMasterData.cs
@@ -0,0 +1,40 @@
+namespace PrintersDataWebMVC.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("PrinterMasterData")]
+    public partial class PrinterMasterData
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string PrinterName { get; set; }
+
+        [StringLength(50)]
+        public string IPAddress { get; set; }
+
+        [StringLength(50)]
+        public string PrintID { get; set; }
+
+        public int? PrinterModeID { get; set; }
+
+        public int BranchID { get; set; }
+
+        [StringLength(200)]
+        public string Description { get; set; }
+
+        public bool? Active { get; set; }
+
+        [StringLength(100)]
+        public string DNSName { get; set; }
+
+        public virtual Branches Branches { get; set; }
+
+        public virtual PrinterModels PrinterModels { get; set; }
+    }
+}
