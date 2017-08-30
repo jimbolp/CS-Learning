726630470a9af2395b58180f673ab276dc36e6ba
Yavor Georgiev <jimbolp@gmail.com>
2017-08-30 18:57:51 +0300

    epplus chart tests

---
+++ Epplus tests/Epplus tests/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epplus_tests
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Documents\Dixel Graphics\Sofia\tests.xlsx";
+            UpdateExcelUsingEPPlus(path);
+        }
+    }
+}
