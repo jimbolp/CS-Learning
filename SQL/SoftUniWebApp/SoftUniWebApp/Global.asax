0e2c2b341ac552675ca9b8e33906a93cd4bd711b
Yavor Georgiev <jimbolp@gmail.com>
2018-04-03 21:47:20 +0300

    stage

--- SQL/SoftUniWebApp/SoftUniWebApp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SoftUniWebApp.MvcApplication" Language="C#" %>
--- firstTest/firstTest/firstTest.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-
-someTestToMake = "some shit to test"
-dateTimeTest = datetime.now()
-print "%s/%s/%s" % (dateTimeTest.day, dateTimeTest.month, dateTimeTest.year)
