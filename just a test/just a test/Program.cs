58b32b1ddeb778837fea06e249b4d87fe7091349
Yavor Georgiev <jimbolp@gmail.com>
2018-05-14 20:55:55 +0300

    hmm

--- just a test/just a test/Program.cs
+++ just a test/just a test/Program.cs
@@ -15,23 +15,7 @@ namespace just_a_test
     {
         static void Main(string[] args)
         {
-            Thread t1 = new Thread(() =>
-            {
-                for (int i = 0; i < 1000; i++)
-                {
-                    Console.WriteLine(i);
-                    Thread.Sleep(1);
-                }
-            });
-            Thread t2 = new Thread(() =>
-            {
-                for (int i = 0; i < 1000; i++)
-                {
-                    Console.WriteLine("Another thread!!");
-                }
-            });
-            t1.Start();
-            t2.Start();
-        }
+           
+        }   
     }
 }
